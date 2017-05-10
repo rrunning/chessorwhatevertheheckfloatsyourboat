dda63f73208b55754d58bd8c8c9edb561094ec26
Samuel Duzett <samuel.duzett@gmail.com>
2017-05-09 19:15:25 -0600

    strip ryan files

--- app/piece.js
+++
@@ -1,7 +0,0 @@
-export default class Piece {
-	constructor(row, col, color) {
-		this.row = row;
-		this.column = col;
-		this.color = color;
-	}
-}
--- app/rook.js
+++
@@ -1,7 +0,0 @@
-
-class Rook extends Piece {
-	constructor(row, col, color) {
-		super(row, col, color);
-		this.image = color === 'white' ? '2656' : '265C';
-	}
-}
