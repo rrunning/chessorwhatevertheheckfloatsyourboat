b71b3b23e9cdc35f54b7060e599f5c4faf2cbb38
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-07 08:24:11 -0600

    bishop movement is started. The squares diagonally to the right of the pieces are being selected but squares to the left aren't yet. The for loops are currently inserting the wrong i into the loops.

--- app/bishop.js
+++ app/bishop.js
@@ -1,8 +1,79 @@
 import Piece from './piece';
+import HelperFunctions from './helper-functions';
 
 export default class Bishop extends Piece {
 	constructor(row, col, color) {
 		super(row, col, color);
 		this.image = color === 'white' ? '2657' : '265D';
+	}
+	getTargetCoordinates() {
+		this.genCoordinates();
+	}
+	genCoordinates() {
+		const potentialMoves = [];
+		for (let i = this.row + 1; i <= 7; i++) {
+			if (HelperFunctions.isEmpty(i, this.col)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
+			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				break;
+			} else {
+				break;
+			}
+		}
+		for (let i = this.row + 1; i <= 7; i++) {
+			if (HelperFunctions.isEmpty(i, this.col)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
+			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				break;
+			} else {
+				break;
+			}
+		}
+		for (let i = this.row - 1; i >= 0; i--) {
+			if (HelperFunctions.isEmpty(i, this.col)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
+			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				break;
+			} else {
+				break;
+			}
+		}
+		for (let i = this.row - 1; i >= 0; i--) {
+			if (HelperFunctions.isEmpty(i, this.col)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
+			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				break;
+			} else {
+				break;
+			}
+		}
+		// for (let i = this.col - 1; i >= 0; i--) {
+		// 	if (HelperFunctions.isEmpty(this.row, i)) {
+		// 		HelperFunctions.addToPotentialMoves(potentialMoves, Number(`${this.row + 1}`), i);
+		// 	} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+		// 		potentialMoves.push({
+		// 			row: this.row,
+		// 			col: i
+		// 		});
+		// 		break;
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+		// for (let i = this.col + 1; i <= 7; i++) {
+		// 	if (HelperFunctions.isEmpty(this.row, i)) {
+		// 		HelperFunctions.addToPotentialMoves(potentialMoves, Number(`${this.row + 1}`), i);
+		// 	} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+		// 		HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+		// 		break;
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+		HelperFunctions.highlightMoves(potentialMoves);
 	}
 }
