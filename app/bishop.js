13837ae4dd15202f676f08f0cac9cd28fd397662
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-08 18:29:44 -0600

    completes the functionality of the bishop piece.

--- app/bishop.js
+++ app/bishop.js
@@ -14,7 +14,7 @@ export default class Bishop extends Piece {
 		for (let i = this.row + 1; i <= 7; i++) {
 			if (HelperFunctions.isEmpty(i, Number(`${this.col + (i - this.row)}`))) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+			} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (i - this.row)}`), this.color)) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
 				break;
 			} else {
@@ -24,7 +24,7 @@ export default class Bishop extends Piece {
 		for (let i = this.row + 1; i <= 7; i++) {
 			if (HelperFunctions.isEmpty(i, Number(`${this.col - (i - this.row)}`))) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+			} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (i - this.row)}`), this.color)) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
 				break;
 			} else {
@@ -34,7 +34,7 @@ export default class Bishop extends Piece {
 		for (let i = this.row - 1; i >= 0; i--) {
 			if (HelperFunctions.isEmpty(i, Number(`${this.col + (this.row - i)}`))) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+			} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (this.row - i)}`), this.color)) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
 				break;
 			} else {
@@ -44,7 +44,7 @@ export default class Bishop extends Piece {
 		for (let i = this.row - 1; i >= 0; i--) {
 			if (HelperFunctions.isEmpty(i, Number(`${this.col - (this.row - i)}`))) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+			} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (this.row - i)}`), this.color)) {
 				HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
 				break;
 			} else {
