703a9705a02f498a49a44bb23d56bd0eec8bbc96
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-11 11:09:00 -0600

    adds knight movement

--- app/knight.js
+++ app/knight.js
@@ -1,10 +1,28 @@
 import Piece from './piece';
+import HelperFunctions from './helper-functions';
 
 export default class Knight extends Piece {
 	constructor(row, col, color) {
 		super(row, col, color);
 		this.image = color === 'white' ? '2658' : '265E';
 	}
+	genCoordinates() {
+		const potentialMoves = [];
+		this.isValidMove(this.row + 2, this.col + 1, potentialMoves);
+		this.isValidMove(this.row + 2, this.col - 1, potentialMoves);
+		this.isValidMove(this.row + 1, this.col - 2, potentialMoves);
+		this.isValidMove(this.row + 1, this.col + 2, potentialMoves);
+		this.isValidMove(this.row - 1, this.col + 2, potentialMoves);
+		this.isValidMove(this.row - 1, this.col - 2, potentialMoves);
+		this.isValidMove(this.row - 2, this.col + 1, potentialMoves);
+		this.isValidMove(this.row - 2, this.col - 1, potentialMoves);
+		return potentialMoves;
+	}
+	isValidMove(row, col, potentialMoves) {
+		if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isEmpty(row, col)) {
+			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
+		} else if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isEnemy(row, col, this.color)) {
+			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
+		}
+	}
 }
-
-// movement - find targets, highlight potential moves
