cafbe3689d8905849304e420cc182df389aec6a4
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-29 18:42:57 -0600

    fixes issue where the king was still able to move inline with the queen's potential moves.

--- app/queen.js
+++ app/queen.js
@@ -8,10 +8,12 @@ export default class Queen extends Piece {
 		super(row, col, color);
 		this.image = color === 'white' ? '2655' : '265B';
 	}
-
+// need to check the bishop forcheck functionality. Sometimes it works and sometimes it doesn't.
+// also need to add allies to the forcheck check. to exclude protected allies
+// from the king's potential moves.
 	genCoordinates(forCheck) {
-		const bTargets = Bishop.prototype.genCoordinates.call(this);
-		const rTargets = Rook.prototype.genCoordinates.call(this);
+		const bTargets = Bishop.prototype.genCoordinates.call(this, forCheck);
+		const rTargets = Rook.prototype.genCoordinates.call(this, forCheck);
 		const potentialMoves = bTargets.concat(rTargets);
 		return potentialMoves;
 	}
--- app/rook.js
+++ app/rook.js
@@ -6,49 +6,106 @@ export default class Rook extends Piece {
 		super(row, col, color);
 		this.image = color === 'white' ? '2656' : '265C';
 	}
-	genCoordinates() {
+	genCoordinates(forCheck) {
 		const potentialMoves = [];
-		for (let i = this.row + 1; i <= 7; i++) {
-			if (HelperFunctions.isEmpty(i, this.col)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
-				break;
-			} else {
-				break;
+		if (forCheck) {
+			for (let i = this.row + 1; i <= 7; i++) {
+				if (HelperFunctions.isEmpty(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color) && HelperFunctions.isKing(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+					break;
+				} else {
+					break;
+				}
 			}
-		}
-		for (let i = this.row - 1; i >= 0; i--) {
-			if (HelperFunctions.isEmpty(i, this.col)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
-			} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
-				break;
-			} else {
-				break;
+			for (let i = this.row - 1; i >= 0; i--) {
+				if (HelperFunctions.isEmpty(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color) && HelperFunctions.isKing(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+					break;
+				} else {
+					break;
+				}
 			}
-		}
-		for (let i = this.col - 1; i >= 0; i--) {
-			if (HelperFunctions.isEmpty(this.row, i)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
-			} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
-				potentialMoves.push({
-					row: this.row,
-					col: i
-				});
-				break;
-			} else {
-				break;
+			for (let i = this.col - 1; i >= 0; i--) {
+				if (HelperFunctions.isEmpty(this.row, i)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color) && HelperFunctions.isKing(this.row, i)) {
+					potentialMoves.push({
+						row: this.row,
+						col: i
+					});
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+					potentialMoves.push({
+						row: this.row,
+						col: i
+					});
+					break;
+				} else {
+					break;
+				}
 			}
-		}
-		for (let i = this.col + 1; i <= 7; i++) {
-			if (HelperFunctions.isEmpty(this.row, i)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
-			} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
-				HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
-				break;
-			} else {
-				break;
+			for (let i = this.col + 1; i <= 7; i++) {
+				if (HelperFunctions.isEmpty(this.row, i)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color) && HelperFunctions.isKing(this.row, i)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+					break;
+				} else {
+					break;
+				}
+			}
+		} else {
+			for (let i = this.row + 1; i <= 7; i++) {
+				if (HelperFunctions.isEmpty(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+					break;
+				} else {
+					break;
+				}
+			}
+			for (let i = this.row - 1; i >= 0; i--) {
+				if (HelperFunctions.isEmpty(i, this.col)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+				} else if (HelperFunctions.isEnemy(i, this.col, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, i, this.col);
+					break;
+				} else {
+					break;
+				}
+			}
+			for (let i = this.col - 1; i >= 0; i--) {
+				if (HelperFunctions.isEmpty(this.row, i)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+					potentialMoves.push({
+						row: this.row,
+						col: i
+					});
+					break;
+				} else {
+					break;
+				}
+			}
+			for (let i = this.col + 1; i <= 7; i++) {
+				if (HelperFunctions.isEmpty(this.row, i)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+				} else if (HelperFunctions.isEnemy(this.row, i, this.color)) {
+					HelperFunctions.addToPotentialMoves(potentialMoves, this.row, i);
+					break;
+				} else {
+					break;
+				}
 			}
 		}
 		return potentialMoves;
