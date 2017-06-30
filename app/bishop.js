9fc8b6be769c8af1a6ab895378556990e3700056
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-29 18:59:54 -0600

    adds functionality of forcheck for bishops. They now consider their allies as targets when checking for potential spaces for the king

--- app/bishop.js
+++ app/bishop.js
@@ -12,9 +12,9 @@ export default class Bishop extends Piece {
 			for (let i = this.row + 1; i <= 7; i++) {
 				if (HelperFunctions.isEmpty(i, Number(`${this.col + (i - this.row)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (i - this.row)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col + (i - this.row)}`))) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col + (i - this.row)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col + (i - this.row)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (i - this.row)}`), this.color)) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col + (i - this.row)}`), this.color)) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (i - this.row)}`));
 					break;
 				} else {
@@ -24,9 +24,9 @@ export default class Bishop extends Piece {
 			for (let i = this.row + 1; i <= 7; i++) {
 				if (HelperFunctions.isEmpty(i, Number(`${this.col - (i - this.row)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (i - this.row)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col - (i - this.row)}`))) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col - (i - this.row)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col - (i - this.row)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (i - this.row)}`), this.color)) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col - (i - this.row)}`), this.color)) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (i - this.row)}`));
 					break;
 				} else {
@@ -36,9 +36,9 @@ export default class Bishop extends Piece {
 			for (let i = this.row - 1; i >= 0; i--) {
 				if (HelperFunctions.isEmpty(i, Number(`${this.col + (this.row - i)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (this.row - i)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col + (this.row - i)}`))) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col + (this.row - i)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col + (this.row - i)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col + (this.row - i)}`), this.color)) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col + (this.row - i)}`), this.color)) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col + (this.row - i)}`));
 					break;
 				} else {
@@ -48,9 +48,9 @@ export default class Bishop extends Piece {
 			for (let i = this.row - 1; i >= 0; i--) {
 				if (HelperFunctions.isEmpty(i, Number(`${this.col - (this.row - i)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (this.row - i)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col - (this.row - i)}`))) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col - (this.row - i)}`), this.color) && HelperFunctions.isKing(i, Number(`${this.col - (this.row - i)}`))) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
-				} else if (HelperFunctions.isEnemy(i, Number(`${this.col - (this.row - i)}`), this.color)) {
+				} else if (HelperFunctions.isAlly(i, Number(`${this.col - (this.row - i)}`), this.color)) {
 					HelperFunctions.addToPotentialMoves(potentialMoves, i, Number(`${this.col - (this.row - i)}`));
 					break;
 				} else {
