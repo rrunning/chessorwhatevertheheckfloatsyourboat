b654ca97fe32dd75bf1dd0ea5d4e12b9920dc196
Ryan Running <ryanrunning@securitymetrics.com>
2017-06-29 18:53:35 -0600

    adds forcheck functionality to knight

--- app/knight.js
+++ app/knight.js
@@ -6,22 +6,40 @@ export default class Knight extends Piece {
 		super(row, col, color);
 		this.image = color === 'white' ? '2658' : '265E';
 	}
-	genCoordinates() {
+	genCoordinates(forCheck) {
 		const potentialMoves = [];
-		this.isValidMove(this.row + 2, this.col + 1, potentialMoves);
-		this.isValidMove(this.row + 2, this.col - 1, potentialMoves);
-		this.isValidMove(this.row + 1, this.col - 2, potentialMoves);
-		this.isValidMove(this.row + 1, this.col + 2, potentialMoves);
-		this.isValidMove(this.row - 1, this.col + 2, potentialMoves);
-		this.isValidMove(this.row - 1, this.col - 2, potentialMoves);
-		this.isValidMove(this.row - 2, this.col + 1, potentialMoves);
-		this.isValidMove(this.row - 2, this.col - 1, potentialMoves);
+		if (forCheck) {
+			this.checkMoves(this.row + 2, this.col + 1, potentialMoves);
+			this.checkMoves(this.row + 2, this.col - 1, potentialMoves);
+			this.checkMoves(this.row + 1, this.col - 2, potentialMoves);
+			this.checkMoves(this.row + 1, this.col + 2, potentialMoves);
+			this.checkMoves(this.row - 1, this.col + 2, potentialMoves);
+			this.checkMoves(this.row - 1, this.col - 2, potentialMoves);
+			this.checkMoves(this.row - 2, this.col + 1, potentialMoves);
+			this.checkMoves(this.row - 2, this.col - 1, potentialMoves);
+		} else {
+			this.isValidMove(this.row + 2, this.col + 1, potentialMoves);
+			this.isValidMove(this.row + 2, this.col - 1, potentialMoves);
+			this.isValidMove(this.row + 1, this.col - 2, potentialMoves);
+			this.isValidMove(this.row + 1, this.col + 2, potentialMoves);
+			this.isValidMove(this.row - 1, this.col + 2, potentialMoves);
+			this.isValidMove(this.row - 1, this.col - 2, potentialMoves);
+			this.isValidMove(this.row - 2, this.col + 1, potentialMoves);
+			this.isValidMove(this.row - 2, this.col - 1, potentialMoves);
+		}
 		return potentialMoves;
 	}
 	isValidMove(row, col, potentialMoves) {
 		if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isEmpty(row, col)) {
 			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
 		} else if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isEnemy(row, col, this.color)) {
+			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
+		}
+	}
+	checkMoves(row, col, potentialMoves) {
+		if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isEmpty(row, col)) {
+			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
+		} else if (HelperFunctions.isOnBoard(row, col) && HelperFunctions.isAlly(row, col, this.color)) {
 			HelperFunctions.addToPotentialMoves(potentialMoves, row, col);
 		}
 	}
